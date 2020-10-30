dc527e88695dbd81b15ff73fc103a34ce72e6825
Ercan Duman✔️ <ercanduman30@gmail.com>
2020-10-30 17:05:19 +0300

    DataSource.kt created to insert initial sample data to mongodb

---
+++ src/main/kotlin/ercanduman/mongodbexample/data/db/datasource/DataSource.kt
@@ -0,0 +1,23 @@
+package ercanduman.mongodbexample.data.db.datasource
+
+import ercanduman.mongodbexample.data.entity.Address
+import ercanduman.mongodbexample.data.entity.Hotel
+import ercanduman.mongodbexample.data.entity.Review
+import ercanduman.mongodbexample.data.repository.AppRepository
+import org.springframework.boot.CommandLineRunner
+import org.springframework.stereotype.Component
+
+@Component
+class DataSource(private val repository: AppRepository) : CommandLineRunner {
+
+    override fun run(vararg args: String?) {
+        // Initialize some random hotels
+        val hotels = listOf(
+                Hotel("Beach Hotel", 125, Address("Marmaris", "Tr"), listOf(Review("Test user", 5, true))),
+                Hotel("Selen Hotel", 150, Address("İçmeler/Marmaris", "Tr"), listOf(Review("Test user 2", 7, true)))
+        )
+
+        // Insert these sample hotels into mongoDb each time server started.
+        repository.saveAll(hotels)
+    }
+}
--- src/main/kotlin/ercanduman/mongodbexample/data/entity/Hotel.kt
+++ src/main/kotlin/ercanduman/mongodbexample/data/entity/Hotel.kt
@@ -1,9 +1,15 @@
 package ercanduman.mongodbexample.data.entity
 
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document(collection = "hotels")
 data class Hotel(
-        val id: String,
         val name: String,
         val price: Int,
         val address: Address,
         val reviews: List<Review>
-) 
+) {
+    @Id
+    var id: String = ""
+}
